a0941a1417e30afbde18d9662b03aa6d050cb32d
srizzo <rizzolabs@gmail.com>
2015-01-19 08:23:26 +0000

    build

--- dist/jqueryajaxlogger.js
+++ dist/jqueryajaxlogger.js
@@ -19,9 +19,9 @@ http://github.com/srizzo/jqueryajaxlogger/* Copyright (c) 2015 Samuel Rizzo; Lic
           .replace(/^\s+at\s+/gm, '')
           .replace(/^Object.<anonymous>\s*\(/gm, '{anonymous}()@')
           .split('\n');
-          var match = ($.grep(stack, function(e){ return !e.match(/jquery[ .\-0-9]*\.js/i) })[1] || "").match(/\(.*\/\/[^\/]+(\/.*)\?body=\d+:(\d+):\d+\)/);
+          var match = ($.grep(stack, function(e){ return !e.match(/jquery[ .\-0-9]*\.js/i) })[1] || "").match(/\(:?.*\/\/[^\/]+(\/.*)/);
       if (match) {
-        console.info(settings.type + " " + settings.url + (settings.data ? (" data: " + settings.data) : "") + " [at " + match[1] + ":" + match[2] + "]");
+        console.info(settings.type + " " + settings.url + (settings.data ? (" data: " + settings.data) : "") + " [from " + match[1] + "]");
       } else {
         console.info(settings.type + " " + settings.url + (settings.data ? (" data: " + settings.data) : ""));
         console.trace();
@@ -30,7 +30,6 @@ http://github.com/srizzo/jqueryajaxlogger/* Copyright (c) 2015 Samuel Rizzo; Lic
   }
   
   jQueryAjaxLogger._active = !jQueryAjaxLogger._active
-  
   if (jQueryAjaxLogger._active) {
     console.info("jQuery Ajax Logger is enabled")
   } else {
